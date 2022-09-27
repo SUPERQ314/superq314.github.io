f083c21f128269b08e5ffa053fb5a262969cee9d
SUPERQ314 <220202090@seu.edu.cn>
2022-09-27 22:40:39 +0800

    Site updated: 2022-09-27 22:39:32

---
+++ js/funny.js
@@ -0,0 +1,18 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
+
+// 其中的 favicon.ico 为正常显示图标，funny.ico 为用户切换后的图标
